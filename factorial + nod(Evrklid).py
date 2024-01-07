d5fcb600dd113ae70f191dee4b4c0c9f6c73c52d
SergeyPlus <splastamakin@bk.ru>
2024-01-07 22:19:25 +0400

    algoritms deleted

--- factorial + nod(Evrklid).py
+++
@@ -1,7 +0,0 @@
-def factorial(n):
-    return 1 if n == 1 else factorial(n - 1) * n
-print('Факториал:', factorial(5))
- 
-def nod(a, b):
-    return min(a, b) if a % b == 0 else nod(b, a % b)
-print('Наименьший общий делитель равен:', nod(12, 11)) 
